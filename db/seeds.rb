f66d22f862e484d2ae2f129fa678b9ad4ad2c1c2
Alex Pritchard <alexbeet1234@gmail.com>
2021-05-30 14:22:52 +0800

    added some bootstrap, created add flat

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,34 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Flat.creat!(
+Flat.create!(
   name: 'Entire house hosted by Salomon',
-  address: '',
-  description: ''
+  address: '92 St Thomas Street',
+  description: 'A lovely family home with a large garden. Perfect for children',
   price_per_night: 100,
-  number_of_guests: 3
+  number_of_guests: 5
+  )
+
+Flat.create!(
+  name: 'Large bright studio flat',
+  address: '2nd street, Soho',
+  description: 'A greate flat situtated in the heart of the city.',
+  price_per_night: 75,
+  number_of_guests: 2
+  )
+
+Flat.create!(
+  name: 'Cheap and chearful apartment',
+  address: 'Upper Street',
+  description: 'Great little place, easy access to shops.',
+  price_per_night: 50,
+  number_of_guests: 2
+  )
+
+Flat.create!(
+  name: 'Old style farm',
+  address: 'Farm road',
+  description: 'A working farm. Get the sights and smells of a real working farm.',
+  price_per_night: 100,
+  number_of_guests: 6
   )
